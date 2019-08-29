ff68c0d0d1c12ed4972ff944d4b2cc5619957c04
vijay kumar <vijay10022@gmail.com>
2019-08-29 16:30:30 +0530

    testing

---
+++ test.py
@@ -0,0 +1,26 @@
+num = []
+for i in range(1, 10):
+    num.append(input('Enter the %s number: '))
+print(max(num))
+
+
+#import numpy as np
+#A=list(map(int,input().split()))
+#A=int(raw_input('Enter the numbers:'))
+#print('max:',max(A))
+
+
+#multi_list=[]
+#multi_list= int(input("Input number of rows: "))
+
+
+
+
+#col_num = int(input("Input number of columns: "))
+#multi_list = [[0 for col in range(col_num)] for row in range(row_num)]
+##multi_list=[]
+##for row in range(row_num):
+##    for col in range(col_num):
+##        multi_list[row][col]= row*col
+##
+##print(multi_list)
